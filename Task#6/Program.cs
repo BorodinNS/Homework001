bafa97bae74d4654bc5743eb941927abb47e449d
Borodin <Fallenlord48@gmail.com>
2022-09-18 17:35:50 +0300

    Решение задачи о определение чётности введенного числа

---
+++ Task#6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+if(num%2 == 0)
+{
+    Console.WriteLine("Число четноё");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
