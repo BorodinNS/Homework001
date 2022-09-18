9a473f875c97005de57cb66472cef083ce1ac8db
Borodin <Fallenlord48@gmail.com>
2022-09-18 17:58:41 +0300

    Решение задачи о выводе всех четных чисел от 0 до введенного числа

---
+++ Task#8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число, которое больше 0: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if(num<=0) Console.WriteLine("Некорректное число, введите число больше 0");
+if(num == 1) Console.WriteLine("В заданном диапозоне нет четных чисел");
+else
+while(num>count)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
