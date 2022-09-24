5cd474f15daaa507047d8fc00dc52a1f7e725a90
Borodin <Fallenlord48@gmail.com>
2022-09-24 14:44:22 +0300

    Будний/выходной день

---
+++ Task#15/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>7) Console.WriteLine("Введите цифру от 1 до 7");
+if(num == 1 || num == 2 || num == 3 || num == 4 || num == 5) 
+    {
+        Console.WriteLine("Будний день");
+    }
+else if (num == 6 || num == 7)
+{
+    Console.WriteLine("Выходной день");
+}
