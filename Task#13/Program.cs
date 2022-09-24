6804cc3a36c2e5ef01278ccd68e8f10a5e32082b
Borodin <Fallenlord48@gmail.com>
2022-09-24 13:09:52 +0300

    С помощью деления выводит третью цифру заданного числа или сообщает, что третьеё цифры нет.

---
+++ Task#13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая с помощью деления выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<100)
+{
+    Console.WriteLine("Третьей цифры нет"); 
+}
+else
+{
+    while(num>999)
+{
+num = (num/10);
+}
+if (num<=999);
+Console.WriteLine(num % 10);
+}
