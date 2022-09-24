ea4876c4df0fbbd6dbd978c13650ac20ba9eaaf9
Borodin <Fallenlord48@gmail.com>
2022-09-24 12:36:58 +0300

    Программа показывает вторую числу введенного трехзначного числа

---
+++ Task#10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int tens;
+if(num > 99 && num < 1000)
+{
+    tens = num / 10 % 10;
+    Console.WriteLine(tens);
+}
+else     
+{
+    Console.WriteLine("Неккоректное число");
+}
